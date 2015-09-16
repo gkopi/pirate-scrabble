f6314016e99e75e5d6f72c281147a76449289e74
Gabriel Kopito <gkopito@gmail.com>
2015-09-15 23:46:21 -0700

    Removed node_modules directory from version control

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});<F37>
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
